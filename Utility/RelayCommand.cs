91f4cdeb5d1d0c70f23ea1ca9f37a7968fb68e2d
Benjamin Leonhardt <bjle71347@edu.ucl.dk>
2025-09-04 13:09:55 +0200

    RealyCommand and BaseViewModdel added

---
+++ Utility/RelayCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ReolMarkedTeam7.Utility;
+
+public class RelayCommand : ICommand
+{
+    private readonly Action<object?> execute;
+    private readonly Func<bool> canExecute;
+
+    public RelayCommand(Action<object?> execute, Func<bool> canExecute = null)
+    {
+        this.execute = execute;
+        this.canExecute = canExecute;
+    }
+
+    public bool CanExecute(object parameter) => canExecute == null || canExecute();
+    public void Execute(object? parameter) => execute(parameter);
+
+    public event EventHandler CanExecuteChanged
+    {
+        add { CommandManager.RequerySuggested += value; }
+        remove { CommandManager.RequerySuggested -= value; }
+    }
+}
