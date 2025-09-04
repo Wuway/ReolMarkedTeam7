3addc943b69673b523678892dfe4bc4345c578f9
Benjamin Leonhardt <bjle71347@edu.ucl.dk>
2025-09-04 14:06:56 +0200

    Initial database connection setup in mainWindow

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Data;
+using System.Data.SqlClient; // Til at arbejde med SQL Server via ADO.NET
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,17 +10,33 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Data.SqlClient; // Til at arbejde med SQL Server via ADO.NET
 
-namespace ReolMarkedTeam7
+namespace ReolMarkedTeam7;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
+        InitializeComponent();
+
+        IConfigurationRoot config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+        string? ConnectionString = config.GetConnectionString("DefaultConnection");
+
+        using (SqlConnection connection = new SqlConnection(ConnectionString))
         {
-            InitializeComponent();
+            connection.Open();
+            MessageBox.Show("Database connection made");
+
         }
+
+
     }
 }
